48e7a7ed734541521a802c4841ce250f7f8de566
Arvind Vishwakarma <165137388+Arvind7703@users.noreply.github.com>
2024-06-13 19:25:55 +0000

    array

---
+++ Container_config/basics_1/array.js
@@ -0,0 +1,13 @@
+let fruits = ["apple","mango","grapes","kiwi"];
+
+// How do you add one or more elements to the end of an array in JavaScript?
+fruits.push("guava")
+// How do you remove the last element from an array in JavaScript?
+fruits.pop()
+// How do you access the first and last elements of an array in JavaScript?
+let startElem = fruits[0];
+let end = (fruits.length)-1
+let endElem = (fruits[end])
+console.log(startElem,endElem);
+// How do you find the length of an array in JavaScript?
+console.log(fruits.length);
