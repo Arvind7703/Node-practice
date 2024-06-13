18f5cad14f36a6aff1b36aa7886247876493ccd0
Arvind Vishwakarma <165137388+Arvind7703@users.noreply.github.com>
2024-06-13 19:52:41 +0000

    functions

---
+++ Container_config/basics_1/functions.js
@@ -0,0 +1,26 @@
+// Define a function that takes two parameters and returns their sum.
+function sum (num1,num2){
+    return num1+num2
+}
+// console.log(sum(2,5));
+
+// Write a function that checks if a number is even or odd.
+
+function odd(num){
+    if(num % 2 === 0){
+        return(`${num} is odd`);
+    }
+    else{
+        return(`${num} is even`);
+    }
+}
+// console.log(odd(12));
+
+// Implement a function that reverses a string.
+function stringReverse (strg){
+    let letter = strg.split("");
+    let reverseLetter = letter.reverse();
+    let reversedStrg = reverseLetter.join("");
+    return reversedStrg
+}
+console.log(stringReverse("Arvind"));
