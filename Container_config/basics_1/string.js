fb1c0198892c66bf1be19fedb119da106fba3eee
Arvind Vishwakarma <165137388+Arvind7703@users.noreply.github.com>
2024-06-13 19:10:30 +0000

    sting

---
+++ Container_config/basics_1/string.js
@@ -0,0 +1,17 @@
+let strg1 = "Arvind";
+let strg2 = " Vishwakarma";
+
+// How do you find the length of a string in JavaScript?
+console.log(strg1.length); // getting lenght of string it also includes spaces(" ")
+// How do you concatenate two strings in JavaScript?
+console.log(strg1 + strg2); // concatination(adding) of string it also includes spaces(" ")
+// How do you extract a substring from a string given a start index and an end index in JavaScript?
+console.log(strg1.substring(0,3)); // trim string using substring
+// How do you convert a string to uppercase in JavaScript?
+console.log(strg1.toLocaleUpperCase()) // making string upper it's we need to call it by using () at the end
+// How do you get the character at a specific index in a string in JavaScript?
+console.log(strg1.charAt(5)) // finding letter character on index 5
+// How do you remove whitespace from both ends of a string in JavaScript?
+console.log(strg2.trim());// trim remove spaces
+// How do you find the index of a specified value in a string in JavaScript?
+console.log(strg1.indexOf("i"));
