75a9c7656f8407a2a9221d04ccce9efc0014127a
Arvind Vishwakarma <165137388+Arvind7703@users.noreply.github.com>
2024-06-13 18:02:46 +0000

    datatypes

---
+++ Container_config/basics_1/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // new version syntax of js
+
+//Primitive or value type 
+
+let name = "Arvind"; // String
+let age = 20;// Number
+let isLogggedin = true // Boolean
+let b = null // standalone value (Empty)
+let a; //undefined
+
+// Non-primitive reference type
+
+let arr = [1,2,3,4] // Array
+let obj = {
+    name:"Arvind",
+    age:20,             //Object
+    isStudent: true,
+}
+
+console.log(typeof a) // type of is to check the data type
---
+++ Container_config/basics_1/variables.js
@@ -0,0 +1,21 @@
+// console.log("test")
+
+// Variables - var,let and const
+
+var name = "Arvind"; //Value can be re-declared
+let age = 20; // Value can not be re-declred
+const dob = "7/7/2003"; // value can not be changed and should intialize at the time of making varible
+
+// example
+
+var a = 5;
+var a = 10;
+//  console.log(a); // output 10 cs we can re-declare
+
+let b = "Arvind";
+let c;// we can also leave it undeclared value we can re assign it later
+c = "Vishwakarma"
+// let b = "change";  it'll trow an error as it can not be re-declared
+
+// const d; it'll throw an error we can not leave const varibel with undeclared value we have to intialize
+const d = "Pass"; // value of const varable can not be changed as it's a constant 
